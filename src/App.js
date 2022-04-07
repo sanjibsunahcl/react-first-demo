13185dc700ec1092a3644d1294494eb28e514c9f
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-04-07 11:00:31 +0530

    Form set up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
-import logo from './logo.svg';
+import { useState ,createContext} from "react";
 import './App.css';
+import MyForm from "./component/form/form";
+
+export const UserContext = createContext();
 
 function App() {
+
+  const [name, setName] = useState("");
+  const [visble, setVisible] = useState(true)
+  const [value, setValue] = useState('Sanjib');
+
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`The name you entered was: ${name}`)
+  }
+
+  const x = 5;
+  const shoot = () => {
+    // alert("Great Shot!");
+    setVisible(true);
+  }
+  const cars = [
+    {id: 1, brand: 'Ford'},
+    {id: 2, brand: 'BMW'},
+    {id: 3, brand: 'Audi'}
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={'https://th.bing.com/th/id/OIP.vMsGWI7Ged8p_0p7VzGD8wHaE7?pid=ImgDet&rs=1'} className="App-logo" alt="logo" />
-        <p>
-          Welcome to react sanjib.
-        </p>
-        <button color='red'>Click</button>
-        <text>gdhh</text>
-      </header>
-    </div>
+    <UserContext.Provider value={[visble, setVisible]}>
+    <div className="App"> 
+       { !visble ?
+      <div>
+        <h2>Hello World!</h2>
+        <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>
+        <button onClick={shoot}>click</button>
+      </div>
+      : null
+      }
+     { visble ? <MyForm></MyForm> : null } 
+  </div>
+  </UserContext.Provider>
+
   );
 }
+
 
 export default App;
