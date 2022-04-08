270a15afde84f51bb3b83202a2e09a07f60f1531
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-04-08 19:23:14 +0530

    Demo set up

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import { useState ,createContext} from "react";
 import './App.css';
 import MyForm from "./component/form/form";
+import MainWeather from "./component/weather/MainWeather";
 
 export const UserContext = createContext();
 
 function App() {
 
   const [name, setName] = useState("");
-  const [visble, setVisible] = useState(true)
+  const [visble, setVisible] = useState(false)
   const [value, setValue] = useState('Sanjib');
+  const [weatherVisible,setWeatherVisible] = useState(false)
 
 
   const handleSubmit = (event) => {
@@ -26,18 +28,35 @@ function App() {
     {id: 2, brand: 'BMW'},
     {id: 3, brand: 'Audi'}
   ];
+  const myStyle={
+    backgroundImage: 
+    "url('https://asia.olympus-imaging.com/content/000090033.jpg')",
+    // height:'100vh',
+    // marginTop:'-70px',
+    fontSize:'30px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+};
   return (
     <UserContext.Provider value={[visble, setVisible]}>
-    <div className="App"> 
+      <div className="App"
+         style={myStyle}
+        > 
        { !visble ?
       <div>
         <h2>Hello World!</h2>
         <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>
-        <button onClick={shoot}>click</button>
+        <button className="btnStyle" onClick={shoot}>Form</button>
+        <div>
+          <button className="btnStyle"
+             onClick={()=>setWeatherVisible(true)}>Weather</button>
+        </div>
       </div>
       : null
       }
      { visble ? <MyForm></MyForm> : null } 
+     {weatherVisible ? <MainWeather/> : null}
+      <div className="view"></div>
   </div>
   </UserContext.Provider>
 
