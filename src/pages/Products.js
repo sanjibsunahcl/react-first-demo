3cacc1143fc768593d6b9360a1bff48ab2e0ed14
sanjibsunahcl <98376370+sanjibsunahcl@users.noreply.github.com>
2022-04-14 15:58:39 +0530

    Multiple page set up

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import '../App.css';
+import MainWeather from '../component/weather/MainWeather';
+import useFetch from '../utils/api';
 
 export default function Products() {
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+  console.log('api Response data'+JSON.stringify(data));
+
   return (
-   <h1 className='products'>PRODUCTS</h1>
+    <div>
+     {/* <h1 className='products'>PRODUCTS</h1> */}
+     <MainWeather/>
+   </div>
    );
 }
