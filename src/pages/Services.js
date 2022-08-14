4c4cb9e97f599e8887446f25ebfe812f04247850
sanjib-suuna <sanjibsuna86@gmail.com>
2022-08-14 16:03:38 +0530

    small changes

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import './services.css';
 import { useNavigate } from 'react-router-dom';
+import CardItem from '../component/CardItem';
 
 
 export default function Services() {
+
+  console.log('home data check');
+
   const navigate = useNavigate();
   return (
+    
     <div className='serViceContainer'>
-      <h1 className='services'>SERVICES</h1> 
+      <h1 className='services'>SERVICES List</h1> 
       <button className='btnStyle' onClick={()=> navigate('/')}>Home</button>
+        <CardItem
+              src='images/img-9.jpg'
+              text='Explore the hidden waterfall deep inside the Amazon Jungle'
+              label='Adventure'
+              path='/services'
+          />
+
+          <p>.</p>
+          <h1>.</h1>
     </div>
     );
   // return (
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,6 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../App.css';
 
 export default function SignUp() {
-  return <h1 className='sign-up'>Sign Up</h1>;
+
+  const [color, setColor] = useState('red');
+
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setColor('black');
+    } else {
+      setColor('white');
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', changeBackground, true);
+    return () => window.removeEventListener('scroll', changeBackground);
+  }, []);
+
+
+
+  return (  
+    <div>
+       <h1 style={{color:color}} onMouseMove>Sign Up</h1>
+       {/* <text>ghdsghhgh</text>  */}
+       <div>
+      <h4 className={color}>Brand</h4>
+      </div>
+    </div>
+  );
 }
